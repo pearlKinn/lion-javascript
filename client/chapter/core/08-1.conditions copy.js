dc65e9bfb0e7567274841079ab36c7642f1ddf0f
pearlKinn <jinju000202@naver.com>
2023-07-06 11:24:07 +0900

    conditons 수업

---
+++ client/chapter/core/08-1.conditions copy.js
@@ -0,0 +1,84 @@
+/* ---------------- */
+/* Condition        */
+/* ---------------- */
+
+/* 
+let result = prompt('자바스크립트의 공식 이름은 무엇일까요?').toLowerCase();
+
+if (result === 'ecmascript') {
+  console.log('정답입니다.');
+} else {
+  console.log('모르셨나요? 정답은 ECMAScript입니다!');
+}
+*/
+
+/* let num = prompt('숫자를 입력해주세요', '0');
+if (num > 0) {
+  console.log(1);
+} else if (num < 0) {
+  console.log(-1);
+} else {
+  console.log(0);
+}
+ */
+
+/* 
+let a = 5;
+let b = 4;
+let result = a + b < 4 ? '미만' : '이상';
+console.log(result); 
+*/
+
+// 그 영화 봤니?
+//     ↓
+// Yes | No
+//     | 영화 볼거니?
+//           ↓
+//       Yes | No
+
+/*
+// 영화 봤니?
+let didWatchMovie = confirm('너 엘리멘탈 영화 봤니?','');
+// 영화 볼거니?
+
+
+
+if(!didWatchMovie){
+  
+  // 영화 볼거니?
+  let goingToWatchMovie = confirm('영화 볼거니?');  
+  
+  if(goingToWatchMovie){
+    let withWho = prompt('누구랑 볼거니?','');
+
+    if(withWho === '여자친구'){
+      console.log('zzz');
+    }else if(withWho === '가족'){
+      console.log('화목하네');
+    }else{
+      console.log('재밌게보구와~~~~');
+    }
+  }
+
+}else{
+  let alone = confirm('너 혼자 봤니?');
+  
+  if(alone){
+    let didCry = confirm('너 울었니..?');
+
+    if(didCry){
+      console.log('너..찌질했네..');
+
+    }else{
+      console.log('너 T야?');
+    }
+  }
+} 
+*/
+// else 절(caluse)
+
+// else if 복수 조건 처리
+
+// 조건부 연산자
+
+// 멀티 조건부 연산자 식
