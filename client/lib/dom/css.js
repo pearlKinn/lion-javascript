d23a1bf2dcc19a15f1263eff7792a91e9a10ed2e
pearlKinn <jinju000202@naver.com>
2023-07-18 15:39:33 +0900

    css (get/set) 유틸함수 생성

--- client/lib/dom/css.js
+++ client/lib/dom/css.js
@@ -1,3 +1,4 @@
+/* Class -------------------------------------------------------------------------- */
 function addClass(node, className) {
   if(typeof node === 'string') node = getNode(node)
   if(typeof className !== 'string') throw new TypeError('addClass 함수의 두 번째 인수는 문자 타입이어야 합니다')
@@ -24,3 +25,31 @@ function toggleClass(node, className) {
   return node.classList.toggle(className)
 }
 
+/* CSS -------------------------------------------------------------------------- */
+
+function setCss(node, prop, value) {
+  if(typeof node === 'string') node = getNode(node)
+
+  if(!(prop in document.body.style)) {
+    throw new SyntaxError('setCss 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다')
+  }
+
+  if(!value) throw new SyntaxError('setCss 함수의 세 번째 인수는 필수값 입니다.')
+
+  if(typeof prop !== 'string' || typeof value !== 'string') throw new TypeError('setCss의 두 번째, 세 번째 타입은 문자이어야 합니다')
+
+  node.style[prop] = value
+}
+
+function getCss(node, prop) {
+  if(typeof node === 'string') node = getNode(node)
+  
+  if(!(prop in document.body.style)) {
+    throw new SyntaxError('getCss 함수의 두 번째 인자인 prop은 유효한 css 속성이 아닙니다')
+  }
+
+  return getComputedStyle(node).getPropertyValue(prop)
+}
+
+// css()
+const css = (node, prop, value) => !value ? getCss(node, prop) : setCss(node, prop, value);
