e87d922f8fa8dd4382a20efddfe1e8df49efa526
pearlKinn <jinju000202@naver.com>
2023-07-27 00:18:52 +0900

    ajax 학습(promise, fetch, async await)

--- client/lib/utils/index.js
+++ client/lib/utils/index.js
@@ -3,5 +3,6 @@
 export * from './xhr.js';
 export * from './copy.js';
 export * from './memo.js';
-export * from './typeOf.js';
+export * from './pearl.js';
 export * from './delay.js';
+export * from './typeOf.js';
--- client/main.js
+++ client/main.js
@@ -1,3 +1,7 @@
-import { xhr } from './lib/index.js';
+import { pearl, xhr, xhrPromise } from './lib/index.js';
 
 // xhr.get('')
+// console.log(xhrPromise);
+// const data = pearl.get('https://jsonplaceholder.typicode.com/users')
+// console.log(data);
+// console.log(await data);
